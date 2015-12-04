e946727e3393a045b97193432c20cca4c158c422
Ana Možina <ana.mozina@gmail.com>
2015-12-04 12:00:24 +0100

    uvoz podatkov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,17 +1,15 @@
 # 2. faza: Uvoz podatkov
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+uvozi.nesrece.mesec <- function() {
+  return(read.csv2("podatki/nesrece-mesec.csv", sep = ";", as.is = TRUE,
+                    skip = 4, fileEncoding = "Windows-1250"))
 }
 
 # Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
+mesec <- uvozi.nesrece.mesec()
 
-obcine <- uvozi.obcine()
+#obcine <- uvozi.obcine()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
